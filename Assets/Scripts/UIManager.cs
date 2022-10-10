75dfae32321855592b89fa3b8dbc46ee156efbd8
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-10-10 15:56:17 +1100

    90 percent partial done

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,6 +6,11 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    private Image innerBarImage;
+    private Transform playerTransform;
+    private Transform playerCanvasT;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,29 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playerTransform != null)
+        {
+            float currentDistance = Mathf.Abs(Mathf.Clamp(playerTransform.position.x, -5.0f, 5.0f));
+            //float currentDistance = playerTransform.position.x;
+            //float absoluteDistance = Mathf.Abs(Mathf.Clamp(currentDistance, -5f, 5f));
+            float currentHP = Mathf.Clamp(1.0f / currentDistance, 0f, 1.0f);
+            Debug.Log("Distance away from zero is " + currentDistance + " and HP would be " + currentHP);
+
+            innerBarImage.fillAmount = currentHP;
+
+            if (currentHP < 0.5f)
+            {
+                innerBarImage.color = Color.red;
+            }
+            else
+            {
+                innerBarImage.color = Color.green;
+            }
+
+            //Transform playerCanvas = innerBarImage.parent.parent;
+            //innerBarImage.rectTransform.rotation = playerCanvasT.rotation;
+        }
+
         
     }
 
@@ -34,6 +62,10 @@ public class UIManager : MonoBehaviour
         if (scene.buildIndex == 1)
         {
             Button quitButton = GameObject.FindWithTag("QuitButton").GetComponent<Button>();
+            innerBarImage = GameObject.FindWithTag("PlayerHealthBar").GetComponent<Image>();
+            playerTransform = GameObject.FindWithTag("Player").GetComponent<Transform>();
+            //playerCanvasT = innerBarImage.parent.parent;
+
             quitButton.onClick.AddListener(QuitGame);
         }
     }
