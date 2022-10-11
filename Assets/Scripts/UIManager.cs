49f51352b5df3e4d01a79d5966366e0d1261e6f9
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-10-11 22:31:42 +1100

    90 percent complete through YT tutorial

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -11,6 +11,7 @@ public class UIManager : MonoBehaviour
     private Transform playerTransform;
     private RectTransform playerCanvasT;
     private Camera mainCamera;
+    //private Quaternion canvasAngle;
     
     // Start is called before the first frame update
     void Start()
@@ -28,7 +29,7 @@ public class UIManager : MonoBehaviour
             //float absoluteDistance = Mathf.Abs(Mathf.Clamp(currentDistance, -5f, 5f));
             float currentHP = Mathf.Clamp((-0.2f * currentDistance) + 1, 0f, 1.0f);
             //float currentHP = Mathf.Clamp(currentDistance / 1.0f + 1, 0f, 1.0f);
-            Debug.Log("Distance away from X zero is " + currentDistance + " and HP would be " + currentHP);
+            //Debug.Log("Distance away from X zero is " + currentDistance + " and HP would be " + currentHP);
 
             innerBarImage.fillAmount = currentHP;
 
@@ -41,17 +42,45 @@ public class UIManager : MonoBehaviour
                 innerBarImage.color = Color.green;
             }
 
+            //canvasAngle.eulerAngles = new Vector3(0f, Mathf.Clamp(mainCamera.transform.rotation.y * 120, -180f, 180f), 0f);
+
+            //Debug.Log(new Vector3(0f, mainCamera.transform.rotation.y, 0f));
+            //Debug.Log(mainCamera.transform.rotation.y);
+
             //innerBarImage.rectTransform.rotation.x = Quaternion.Euler(45f, 0f, 0f);
 
             //playerCanvasT = playerTransform.Find("PlayerCanvas")..GetComponent<RectTransform>();
-            //playerCanvasT.rotation.y = mainCamera.transform.rotation.y;
+            //playerCanvasT.rotation.y = mainCamera.transform.rotation.y * 100;
+            //playerCanvasT.rotation = Quaternion.Euler(new Vector3(0f, mainCamera.transform.rotation.y * 100, 0f));
+            //playerCanvasT.rotation = Quaternion.Euler(0f, mainCamera.transform.rotation.y * 100, 0f);
+            //playerCanvasT.rotation = canvasAngle;
+            //playerCanvasT.localEulerAngles = new Vector3(0f, Mathf.Clamp(mainCamera.transform.rotation.y * 120, -180f, 180f), 0f);
             //playerCanvasT.LookAt(mainCamera.transform.rotation.y);
             //playerCanvasT.Rotate(0f, mainCamera.transform.rotation.y, 0f);
-            playerCanvasT.SetPositionAndRotation(playerCanvasT.position, Quaternion.Euler(new Vector3(0f, mainCamera.transform.rotation.y, 0f)));// this one seems be working fine
+            //playerCanvasT.SetPositionAndRotation(playerCanvasT.position, Quaternion.Euler(new Vector3(0f, mainCamera.transform.rotation.y, 0f)));// this one seems be working fine
             //innerBarImage.rectTransform.rotation = playerCanvasT.rotation;
+            //playerCanvasT.LookAt(mainCamera.GetComponent<Transform>());
+
+            //Debug.Log(playerCanvasT.rotation);
+
+            //playerCanvasT.LookAt(mainCamera.transform);
+            
         }
 
-        
+
+    }
+
+    void LateUpdate()
+    {
+        if (playerTransform != null)
+        {
+            playerCanvasT.rotation = mainCamera.transform.rotation;
+            playerCanvasT.rotation = Quaternion.Euler(0f, playerCanvasT.rotation.eulerAngles.y, 0f);
+
+            // I spend two days on the billboarding of HP bar and could not figure out anything, so I looked up some youtube tutorials. 
+            // How To... Billboarding in Unity 2020 - 2D Sprites in 3D, made by gamesplusjames https://www.youtube.com/watch?v=_LRZcmX_xw0. 
+        }
+
     }
 
     public void LoadFirstLevel()
